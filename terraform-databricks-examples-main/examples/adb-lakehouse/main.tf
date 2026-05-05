0b0ddbb4632cf0b0b4d23d995cacb24f866d7c77
joe.barth <joe.barth@evocs.tech>
2026-05-05 19:16:59 +0000

    readd metastore

--- terraform-databricks-examples-main/examples/adb-lakehouse/main.tf
+++ terraform-databricks-examples-main/examples/adb-lakehouse/main.tf
@@ -1,3 +1,16 @@
+module "adb-lakehouse-uc-metastore" {
+  source                     = "../../modules/adb-uc-metastore"
+  metastore_storage_name     = var.metastore_storage_name
+  metastore_name             = var.metastore_name
+  access_connector_name      = var.access_connector_name
+  shared_resource_group_name = var.shared_resource_group_name
+  location                   = var.location
+  tags                       = var.tags
+  providers = {
+    databricks = databricks.account
+  }
+}
+
 module "adb-lakehouse" {
   # With UC by default we need to explicitly create a UC metastore, otherwise it will be created automatically
   depends_on                      = [module.adb-lakehouse-uc-metastore]
