19c9a8a8a65fc1269e0596f5fe644d689f35c24b
joe.barth <joe.barth@evocs.tech>
2026-05-07 13:49:51 +0000

    moved storage creation up

--- datalake/main.tf
+++ datalake/main.tf
@@ -1,7 +1,23 @@
 data "azurerm_resource_group" "existing" {
   name = var.resource_group_name
 }
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
 
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  min_tls_version          = "TLS1_2"
+  allow_blob_public_access = false
+}
+resource "azurerm_storage_container" "container" {
+  name                  = "unity-catalog"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
 resource "azurerm_databricks_access_connector" "this" {
   name                = var.access_connector_name
   resource_group_name = data.azurerm_resource_group.existing.name
@@ -67,23 +83,4 @@ resource "azurerm_role_assignment" "uc_storage_access" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_databricks_access_connector.this.identity[0].principal_id
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
-
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-
-  min_tls_version          = "TLS1_2"
-  allow_blob_public_access = false
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = "unity-catalog"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
 }
