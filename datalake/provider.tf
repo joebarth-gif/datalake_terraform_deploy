03f6285eb7d58f662d8f3b815e6b098888e2735c
joe.barth <joe.barth@evocs.tech>
2026-05-08 01:30:14 +0000

    test

--- datalake/provider.tf
+++ datalake/provider.tf
@@ -29,3 +29,12 @@ provider "databricks" {
   azure_client_secret = var.azure_client_secret
   azure_tenant_id     = var.azure_tenant
 }
+
+provider "databricks" {
+  alias = "workspace"
+  host  = azurerm_databricks_workspace.this.workspace_url
+
+  azure_client_id     = var.azure_client_id
+  azure_client_secret = var.azure_client_secret
+  azure_tenant_id     = var.azure_tenant
+}
