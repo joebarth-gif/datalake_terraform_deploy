5be1340cd2233a1388788fe7ce281c16e8252255
joe.barth <joe.barth@evocs.tech>
2026-05-08 02:16:09 +0000

    remove uc cred

--- datalake/main.tf
+++ datalake/main.tf
@@ -62,20 +62,7 @@ resource "azurerm_role_assignment" "uc_storage_access" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_databricks_access_connector.this.identity[0].principal_id
 }
-resource "databricks_storage_credential" "credential" {
-  provider = databricks.workspace
 
-  name = "uc-credential"
-
-  azure_managed_identity {
-    access_connector_id = azurerm_databricks_access_connector.this.id
-  }
-
-  depends_on = [
-    azurerm_role_assignment.uc_storage_access,
-    databricks_metastore_assignment.assignment
-  ]
-}
 resource "databricks_external_location" "location" {
   provider = databricks.workspace
 
